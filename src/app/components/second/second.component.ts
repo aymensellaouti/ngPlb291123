cbf35a9a3b53a032b61c6bbdf8717a3b6feded79
aymensellaouti <aymen.sellaouti@gmail.com>
2023-11-29 12:14:38 +0100

    exemple binding

---
+++ src/app/components/second/second.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-second',
+  template: `
+    <div class="alert alert-success">
+      <p>second works!</p>
+</div>
+  `,
+  styles: [
+  ]
+})
+export class SecondComponent {
+
+}
