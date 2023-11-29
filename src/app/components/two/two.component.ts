b91177e9b736e59a880d6e011b12a89ff4b349cb
aymensellaouti <aymen.sellaouti@gmail.com>
2023-11-29 14:11:07 +0100

    two way databinding

---
+++ src/app/components/two/two.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-two',
+  templateUrl: './two.component.html',
+  styleUrls: ['./two.component.css']
+})
+export class TwoComponent {
+  two = 'test';
+
+  initTwo() {
+    this.two = 'updated value';
+  }
+}
